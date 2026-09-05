ceb37d8441359c0bcd5612b74b5df1b7f06423fc
Z1058 <omarnairoukh10@gmail.com>
2026-09-05 03:20:58 +0100

    Implement minimax search and heuristic evaluation

--- chess-engine.py
+++ chess-engine.py
@@ -1,6 +1,74 @@
 import chess
 import random
 import tkinter as tk
+import time
+nodesearched = 0
+
+def minimax(board, depth):
+    global nodesearched
+    nodesearched += 1
+    if depth == 0:
+        return evaluate(board)
+    turn = board.turn
+    if turn == chess.WHITE:
+        bestscore = float("-inf")
+        for move in board.legal_moves:
+            board.push(move)
+            score = minimax(board, depth - 1)
+            board.pop()
+            if score > bestscore:
+                bestscore = score
+        return bestscore
+
+    elif turn == chess.BLACK:
+        bestscore = float("inf")
+        for move in board.legal_moves:
+            board.push(move)
+            score = minimax(board, depth - 1)
+            board.pop()
+            if score < bestscore:
+                bestscore = score
+        return bestscore
+
+
+def blackmove(board):
+    global nodesearched
+    nodesearched = 0
+    starttime = time.perf_counter()
+    bestscore = float("inf")
+    bestmove = None
+    for move in board.legal_moves:
+        board.push(move)
+        tempscore = minimax(board, 2)
+        board.pop()
+
+        if tempscore < bestscore:
+            bestscore = tempscore
+            bestmove = move
+    searchtime = time.perf_counter() - starttime
+
+    print(f"Engine move: {bestmove}")
+    print(f"Evaluation: {bestscore}")
+    print(f"Positions searched: {nodesearched}")
+    print(f"Search time: {round(searchtime, 3)}s")
+    return bestmove
+
+def evaluate(board):
+    score = 0
+    piecevalues = {chess.PAWN: 100, chess.KNIGHT: 300, chess.BISHOP: 300, chess.ROOK: 500, chess.QUEEN: 900}
+    for piecetype, value in piecevalues.items():
+        whitecount = len(board.pieces(piecetype, chess.WHITE))
+        blackcount = len(board.pieces(piecetype, chess.BLACK))
+        score += piecevalues[piecetype] * whitecount - piecevalues[piecetype] * blackcount
+    centresquares = [chess.D4, chess.E4, chess.D5, chess.E5]
+    for square in centresquares:
+        piece = board.piece_at(square)
+        if piece is not None:
+            if piece.color ==  chess.WHITE:
+                score += 20
+            else:
+                score -= 20
+    return score
 
 def getsquare(event):
     global selectedsquare
@@ -20,10 +88,14 @@ def getsquare(event):
             board.push(cmove)
             drawboard()
             selectedsquare = None
+            bmove = blackmove(board)
+            if bmove is not None:
+                board.push(bmove)
+                drawboard()
+                print(evaluate(board))
         else:
             print("Illegal move!")
             selectedsquare = None
-
 
 def drawboard():
     canvas.delete("all")
@@ -48,7 +120,6 @@ def drawboard():
 squaresize = 640 // 8
 selectedsquare = None
 
-
 window = tk.Tk()
 window.title("Explainable Chess Engine")
 
@@ -60,5 +131,6 @@ drawboard()
 canvas.bind("<Button-1>", getsquare)
 
 print(board)
+print(evaluate(board))
 
 window.mainloop()
